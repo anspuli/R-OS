e99ca9e48cb0056c052e502c1440bebd7a1805a9
Rigved <71826040+RgTheGreat@users.noreply.github.com>
2020-12-27 00:13:43 +0530

    Create start.py

---
+++ start/demo/start.py
@@ -0,0 +1,12 @@
+username = input("Enter new username: ")
+print("your new Username is: " + username)
+# will print the username and puts in a list
+
+password = input("Enter new password for" + username + ":")
+print("new password is: " + password)
+
+# list
+data = [username, password]
+print(data)
+print("Your length of user and pswd is : ")
+print(len(data))
